df71f8e9d44c315d818281a8a35f08dc7085ab03
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-03-11 15:20:47 +0100

    Add tooltip on navbar icons

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -31,38 +31,41 @@ export default class Navbar extends React.Component {
         return (
             <div id="navbar-wrapper">
                 <nav>
-                    <ul>
-                        <li>
+                    <ul className="nav-icons">
+                        <li className="nav-icon">
                             <img className={this.props.currentContent === "about-me" ? "active-page" : "normal-page"}
                                 src={process.env.PUBLIC_URL + "/icons/navbar/user.svg"}
                                 onClick={() => this.props.changeContent("about-me")}
                                 alt="Icon for section About Me" />
+                            <span>Profil</span>
                         </li>
-                        <li>
+                        <li className="nav-icon">
                             <img className={this.props.currentContent === "skills" ? "active-page" : "normal-page"}
                                 src={process.env.PUBLIC_URL + "/icons/navbar/coding.svg"}
                                 onClick={() => this.props.changeContent("skills")}
                                 alt="Icon for section Skills" />
+                            <span>Compétences</span>
                         </li>
-                        <li>
+                        <li className="nav-icon">
                             <img className={this.props.currentContent === "experiences" ? "active-page" : "normal-page"}
                                 src={process.env.PUBLIC_URL + "/icons/navbar/experience.svg"}
                                 onClick={() => this.props.changeContent("experiences")}
                                 alt="Icon for section Experiences" />
-
+                            <span>Expérience</span>
                         </li>
-                        <li>
+                        <li className="nav-icon">
                             <img className={this.props.currentContent === "projects" ? "active-page" : "normal-page"}
                                 src={process.env.PUBLIC_URL + "/icons/navbar/projects.svg"}
                                 onClick={() => this.props.changeContent("projects")}
                                 alt="Icon for section Projects" />
-
+                            <span>Projets</span>
                         </li>
-                        <li>
+                        <li className="nav-icon">
                             <img className={this.props.currentContent === "contact" ? "active-page" : "normal-page"}
                                 src={process.env.PUBLIC_URL + "/icons/navbar/contact.svg"}
                                 onClick={() => this.props.changeContent("contact")}
                                 alt="Icon for section Contact" />
+                            <span>Contact</span>
                         </li>
                     </ul>
                 </nav>
