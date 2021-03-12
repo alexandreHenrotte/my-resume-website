28eed220f2af9d9f8ac5d924d845184f1605d043
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-03-12 15:06:58 +0100

    Add fade-in animation

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -2,9 +2,29 @@ import React from 'react';
 import './Content.css';
 
 export default class Content extends React.Component {
+    
+    constructor() {
+        super()
+        
+        this.state = {
+            created: false
+        }
+    }
+
+    fadeIn = () => {
+        var content = document.getElementById("content");
+        content.classList.add("fade-in");
+        
+    }
+    onAnimationEnd = () => {
+        var content = document.getElementById("content");
+        content.classList.remove("fade-in");
+    }
+
     render() {
+        this.state.created ? this.fadeIn() : this.setState({created: true});
         return (
-            <body id="content">
+            <body id="content" onAnimationEnd={this.onAnimationEnd}>
                 {this.props.children}
             </body>
         )
