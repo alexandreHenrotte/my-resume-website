3601feed7e0a3df502dd726fd38409fc3962b220
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-04-21 12:25:35 +0200

    Fix image path

--- src/Components/Pages/Projects/Projects.js
+++ src/Components/Pages/Projects/Projects.js
@@ -10,7 +10,7 @@ export default class Projects extends React.Component {
                     title="Belgium Covid Tracker"
                     githubProjectName="alexandreHenrotte/belgium-covid-tracker"
                     img= {{
-                        src: process.env.PUBLIC_URL + "images/projects/belgium-covid-tracker.jpg",
+                        src: process.env.PUBLIC_URL + "/images/projects/belgium-covid-tracker.jpg",
                         alt: "Belgium Covid Tracker Project"
                     }}
                     text="Bot Twitter postant journalièrement des statistiques sur l'état du Covid-19 en Belgique."
@@ -19,7 +19,7 @@ export default class Projects extends React.Component {
                     title="Stock Manager (en cours)"
                     githubProjectName="alexandreHenrotte/stock-manager"
                     img= {{
-                        src: process.env.PUBLIC_URL + "images/projects/stock-manager.jpg",
+                        src: process.env.PUBLIC_URL + "/images/projects/stock-manager.jpg",
                         alt: "Stock Manager Project"
                     }}
                     text="Site web permettant aux utilisateurs la création de stocks et la gestion de leurs produits."
